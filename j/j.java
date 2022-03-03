82ab89b5dbbb7c161898d7b9c1953808cecf0223
willjedynak <willmail@thejedynaks.com>
2022-03-02 16:47:23 -0800

    ignore this commit

--- j/j.java
+++ j/j.java
@@ -25,25 +25,13 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 
-import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
-
 public class j extends Application {
-	int b=0;
 	//public void start(Stage primaryStage) {
 	public BorderPane TEST() {
 		Pane pane = new HBox(10);
 		Button bt = new Button("Center",new ImageView("png.png"));
 		pane.getChildren().addAll(bt);
-		//pane.setAlignment(Pos.CENTER);
+		pane.setAlignment(Pos.CENTER);
 		pane.setStyle("-fx-border-color: green");
 
 		BorderPane pain = new BorderPane();
@@ -56,70 +44,9 @@ public class j extends Application {
 		bt.setOnAction(e -> System.out.println("It worked."));
 		return pain;
 	}
-	public Button imgButton(String in){
-		//Button bt = new Button("",new ImageView(in));
-		ImageView image = new ImageView(in);
-		Button bt = new Button("",image);
-		//bt = new Button("",new ImageView("flop.png"));
-		//bt.setOnAction(e -> System.out.println("It worked."));
-		//bt.setOnAction(e -> bt = new Button("",new ImageView("flop.png")));
-		bt.setOnAction(e -> flip(image));
-		return bt;
-	}
-	public final void flip(ImageView in){
-		System.out.println("We tried.");
-		//in = new Button("",new ImageView("flop.png"));
-		if ((b++)%2==0)
-			in.setImage(new Image("flop.png"));
-		else
-			in.setImage(new Image("flip.png"));
-	}
-	public final void flop(){
-		System.out.println("Flop.");
-	}
 	@Override
 	public void start(Stage primaryStage) {
-		//Scene scene = new Scene(TEST(),450,200);
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
-		//pane.setAlignment(Pos.CENTER);
-		pane.setPadding(new Insets(11.5,12.5,13.5,14.5));
-		pane.setHgap(5.5);
-		pane.setVgap(5.5);
-
-		//pane.add(new Label("First Name:"), 0, 0);
-		pane.add(imgButton("png.png"), 0, 0);
-		pane.add(imgButton("png.png"), 0, 1);
-		pane.add(imgButton("png.png"), 0, 2);
-		pane.add(imgButton("png.png"), 1, 0);
-		pane.add(imgButton("png.png"), 1, 1);
-		pane.add(imgButton("png.png"), 1, 2);
-		pane.add(imgButton("png.png"), 2, 0);
-		pane.add(imgButton("png.png"), 2, 1);
-		pane.add(imgButton("png.png"), 2, 2);
-		/*
-		pane.add(new Button("",new ImageView("png.png")), 0, 0);
-		pane.add(new Button("",new ImageView("png.png")), 0, 1);
-		pane.add(new Button("",new ImageView("png.png")), 0, 2);
-		pane.add(new Button("",new ImageView("png.png")), 1, 0);
-		pane.add(new Button("",new ImageView("png.png")), 1, 1);
-		pane.add(new Button("",new ImageView("png.png")), 1, 2);
-		pane.add(new Button("",new ImageView("png.png")), 2, 0);
-		pane.add(new Button("",new ImageView("png.png")), 2, 1);
-		pane.add(new Button("",new ImageView("png.png")), 2, 2);
-		*/
-		/*
-		pane.add(new TextField(), 1, 0);
-		pane.add(new Label("MI:"), 0, 1);
-		pane.add(new TextField(), 1, 1);
-		pane.add(new Label("Last Name:"), 0, 2);
-		pane.add(new TextField(), 1, 2);
-		Button btAdd = new Button("Add Name");
-		pane.add(btAdd, 1, 3);
-		GridPane.setHalignment(btAdd, HPos.RIGHT);
-		*/
-
-		Scene scene = new Scene(pane);
+		Scene scene = new Scene(TEST(),450,200);
 		primaryStage.setTitle("Show image.");
 		primaryStage.setScene(scene);
 		primaryStage.show();
--- j/j/c.java
+++ j/j/c.java
@@ -1,171 +1,11 @@
 import java.io.*;
 import java.net.*;
-import javafx.application.*;
-import javafx.stage.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.image.*;
-import javafx.geometry.*;
-	import javafx.scene.text.*;
-import javafx.scene.*;
+//import javafx.application.Application;
+import java.util.Scanner;
 
-class myButton extends Button {
-	int i; // Index. 
-	ImageView thisImage;
-	void setImage(String in){
-		thisImage.setImage(new Image(in));
-	}
-	myButton(String tmp, ImageView lst,int i){
-		super(tmp,lst);
-		this.i = i;
-		thisImage = lst;
-	}
-	void onClick(){
-		if (c.yourTurn)
-			yeet(i);
-		  else return; // Is this even necessary? 
-	}
-	void yeet(int i){
-		System.out.println("Yeet.");
-// I wonder if the compiler is smart enough to work this one out. 
-		int tmp=1;
-		for (int j=0;j<i;++j)
-			tmp *= 3;
-// I wonder if the compiler is smart enough to work this one out. 
-		if (c.b/tmp%3==0)
-			c.b += tmp*2;
-		else
-			System.out.println("At that location it's-" );
-		c.yourTurn = false;
-		c.showBoard(c.b);
-	}
-}
-public class c extends Application {
-	static boolean yourTurn = true;
-	static int b=0; // The state of the board. 
-	public myButton imgButton(String in, int i){
-		ImageView image = new ImageView(in); // Move this within class? 
-		 image.setPreserveRatio(true);
-		 image.setFitHeight(200);
-		myButton bt = new myButton("",image,i);
-		bt.setOnAction(e -> bt.onClick());
-		//bt.setOnAction(e -> b = 777);
-		return bt;
-	}
-
-	myButton[] myButtons = new myButton[9];
-
-	void refreshBoard(){
-		for (int i=0;i<9;++i)
-		{
-			int I = i+1;
-			int tmp = 1;
-			for (int j=0;j<i;++j)
-				tmp *= 3;
-			if ((b/tmp)%3 == 1)
-				myButtons[i].setImage("flip.png");
-			else if ((b/tmp)%3 == 2)
-				myButtons[i].setImage("flop.png");
-			//else continue;
-		}
-	}
-	
-	@Override
-	public void start(Stage primaryStage) {
-		System.out.println("In start.");
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
-		for (int i=0;i<9;++i)
-		{
-			myButtons[i] = imgButton("blank.png",i);
-			pane.add(myButtons[i],i%3,i/3);
-		}
-		 primaryStage.setTitle("c.java");
-		primaryStage.setScene(new Scene(pane));
-		System.out.println("Executing show.");
-		primaryStage.show();
-		System.out.println("Executed show.");
-		//new Thread(() -> {while(true)System.out.println("b: " + b);});
-		//new Thread(() -> {while(true)System.out.println("b: " + b);});
-		new Thread(() -> {
-		// Have to use new thread. 
-			try {
-				System.out.println("Hallo, moto!");
-				int port = 8000;
-				String host = "localhost";
-				DataInputStream in;
-				DataOutputStream out;
-				Socket socket;
-				socket = new Socket(host, port);
-				in = new DataInputStream(socket.getInputStream());
-				out = new DataOutputStream(socket.getOutputStream());
-				while (true)
-				{
-					out.writeInt(b);
-					int tmp = in.readInt();
-					yourTurn = (yourTurn || tmp != b); // TMHWW. 
-					if (b > tmp) // DON'T TOUCH THIS. 
-						yourTurn = false; // DON'T TOUCH THIS. 
-					refreshBoard();
-					//b = (tmp > b)? tmp : b;
-					if (tmp > b)
-						b = tmp;
-					//,showBoard(b);
-					/*
-					if (b == 13 *2|| b == 13*27 *2|| b == 13*27*27  *2||  b == 757 *2|| b == 757*3 *2|| b == 757*3*3  *2||  b == (1+81+(729*9)) *2|| b == (9+81+729)*2)
-					{
-						System.out.println("You won!");
-						System.exit(1);
-					}
-					*/
-					CheckIfWon(2);
-				}
-			}
-			catch(IOException ex){
-				System.out.println("Error.");
-				System.out.println("You know, that probably means you lost!");
-				System.out.println(ex);
-				  System.exit(2);
-			}
-		}).start();
-	}
-	void CheckIfWon(int in){
-		if (checkIfWon(in))
-			{
-				System.out.println("You won!");
-				System.exit(1);
-			}
-	}
-	boolean checkIfWon(int in){
-		int B = b;
-		for (int i=1;i<=3;++i)
-		{
-			if (B%27 == 13*in)
-				return true;
-			B /= 27;
-		}
-		/*
-		if (B%3 == in && (B/27)%3 == (B/(27*27))%3)// && (B/27)%3 == in)
-			return true;
-		*/
-		B = b;
-		for (int i=1;i<=9;i*=3)
-		{
-			if ((B%(i*3) == (B/27)%(i*3)) && (B/(27*27)%(i*3) == in) && (in == B%(i*3))) // This might break. 
-				return true;
-		}
-		//                                                          Right diagonal \/. 
-		if ((b%3 == (b/81)%3 && (b/6561)%3 == in && in == b%3) || ((b/9)%3 == (b/81)%3 && (b/729)%3 == in && in == (b/9)%3))
-		// Left diagonal ^. 
-			return true;
-		// I'd really love to see what the compiler made out of this one. 
-		return false;
-	}
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-		Application.launch(args);
-	}
-	public static void showBoard(int b){
+public class c {
+	static int B=0;
+	static void showBoard(int b){
 		for (int i=1;i<=9;++i)
 		{
 			//char tmp = (char)((b/(i*3))%3);
@@ -185,5 +25,28 @@ public class c extends Application {
 			b /= 3;
 		}
 	}
+	public static void main(String[] args){
+		try{
+			System.out.println("Hello, world!");
+			int port = 8000;
+			String host = "localhost";
+			DataInputStream in;
+			DataOutputStream out;
+			Socket socket;
+			socket = new Socket(host, port);
+			in = new DataInputStream(socket.getInputStream());
+			out = new DataOutputStream(socket.getOutputStream());
+			Scanner inp = new Scanner(System.in);
+			while (true)
+			{
+				//System.out.println(in.readInt());
+				B = in.readInt();
+				showBoard(B);
+				out.writeInt(B);
+			}
+		}
+		catch(IOException ex){
+			System.out.println("Error.");
+		}
+	}
 }
-
